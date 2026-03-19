84eee9ba52ad51d105da665bd979932824f669d3
Tiare Salazar <VI-Alumno@duoc.acad>
2026-03-18 21:09:36 -0300

    Se agrega todo

---
+++ src/main/java/cl/duoc/bibliotecaduoc/BibliotecaduocApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.bibliotecaduoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaduocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaduocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/bibliotecaduoc/BibliotecaduocApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.bibliotecaduoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaduocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
